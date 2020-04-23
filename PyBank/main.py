6b995682dfb8e3167de88dbdd79b69620c10b7d5
SethAbbott916 <sethcode@gmail.com>
2020-04-22 21:48:11 -0700

    total months complete

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,11 +1,15 @@
 import os
 import csv
-
+totalmonths = 0
 budgetpath = os.path.join("Resources", "budget_data.csv")
 
 with open (budgetpath) as bankfile:
 
-    csvreader = csv.reader(bankfile, delimiter=',')
-    print(csvreader)
-    for row in csvreader:
+    datareader = csv.reader(bankfile, delimiter=',')
+    print(datareader)
+    
+    for row in datareader:
         print(row)
+    
+        totalmonths = totalmonths + 1
+        print(f"Total Months: {totalmonths}")
